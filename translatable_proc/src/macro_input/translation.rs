3d7f79c160671b5855321d941eda4a8161e3dfdf
stifskere <esteve@memw.es>
2025-04-08 08:10:34 +0200

    feat: implemented thiserror to parse `translatable!` macro input in a private maneer

--- translatable_proc/src/macro_input/translation.rs
+++ translatable_proc/src/macro_input/translation.rs
@@ -5,15 +5,35 @@ use quote::ToTokens;
 use syn::token::Static;
 use syn::{parse2, Error as SynError, LitStr, Path, PathArguments, Result as SynResult, Token, Ident};
 use syn::parse::{Parse, ParseStream};
+use thiserror::Error;
 use translatable_shared::Language;
 
 use super::input_type::InputType;
+
+/// Used to represent errors on parsing a `TranslationMacroArgs`
+/// using `parse_macro_input!`.
+///
+/// The enum implements a helper function to convert itself
+/// to a `syn` spanned error, so this enum isn't directly
+/// exposed as the `syn::Error` instance takes place.
+#[derive(Error, Debug)]
+enum TranslationMacroArgsError {
+    /// An error while parsing a compile-time String value
+    /// was found.
+    #[error("The literal '{0}' is an invalid ISO 639-1 string, and cannot be parsed")]
+    InvalidIsoLiteral(String),
+
+    /// Extra tokens were found while parsing a static path for
+    /// the `translation!` macro, specifically generic arguments.
+    #[error("This translation path contains generic arguments, and cannot be parsed")]
+    InvalidPathContainsGenerics
+}
 
 /// The `TranslationMacroArgs` struct is used to represent
 /// the `translation!` macro parsed arguments, it's sole
 /// purpose is to be used with `parse_macro_input!` with the
 /// `Parse` implementation the structure has.
-struct TranslationMacroArgs {
+pub struct TranslationMacroArgs {
     /// Represents the user specified language
     /// which may be static if the specified language
     /// is a string literal or a `Language` enum tagged
@@ -37,6 +57,12 @@ struct TranslationMacroArgs {
     replacements: HashMap<String, TokenStream2>,
 }
 
+impl TranslationMacroArgsError {
+    pub fn into_syn_error<T: ToTokens>(self, span: T) -> SynError {
+        SynError::new_spanned(span, self.to_string())
+    }
+}
+
 /// The implementation is used to achieve the
 /// sole purpose this structure has, which is being
 /// used in a `parse_macro_input!` call.
@@ -47,10 +73,10 @@ impl Parse for TranslationMacroArgs {
             Ok(literal) => match literal.value().parse::<Language>() {
                 Ok(language) => InputType::Static(language),
 
-                Err(_) => Err(SynError::new_spanned(
-                    literal,
-                    "This literal is an invalid ISO 639-1 string and cannot be parsed."
-                ))?
+                Err(_) => Err(
+                    TranslationMacroArgsError::InvalidIsoLiteral(literal.value())
+                        .into_syn_error(literal)
+                    )?
             },
 
             Err(_) => InputType::Dynamic(language_arg)
@@ -67,10 +93,10 @@ impl Parse for TranslationMacroArgs {
                     .map(|segment| match segment.arguments {
                         PathArguments::None => Ok(segment.ident.to_string()),
 
-                        other => Err(SynError::new_spanned(
-                            other,
-                            "A translation doesn't have generic arguments"
-                        )),
+                        other => Err(
+                            TranslationMacroArgsError::InvalidPathContainsGenerics
+                                .into_syn_error(other)
+                        ),
                     })
                     .collect::<Result<Vec<_>, _>>()?;
 
